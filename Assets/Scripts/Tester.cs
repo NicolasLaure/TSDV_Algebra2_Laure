6f0e48b04118d9e66d678afa1326b9d2e2c5ec04
Nicodragon25 <nicolasariellaure@gmail.com>
2024-08-23 12:49:09 -0300

    Add tester class

---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using CustomMath;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    [SerializeField] private Transform unityTransform;
+    [SerializeField] private MyTransform _transform;
+
+
+    [ContextMenu("Test")]
+    private void Test()
+    {
+        Debug.Log(unityTransform.localToWorldMatrix + "\n" + _transform.localToWorldMatrix);
+        Debug.Log(unityTransform.position);
+        Debug.Log(unityTransform.rotation);
+        Debug.Log(unityTransform.lossyScale);
+    }
+}
